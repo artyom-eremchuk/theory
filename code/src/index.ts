a0e0f1b0a36671d0a6de7d370f6f2619c3f39059
Artyom Eremchuk <artyom.eremchuk.it@gmail.com>
2024-08-12 12:18:45 +0300

    add fizzbuzz

--- code/src/index.ts
+++ code/src/index.ts
@@ -2,5 +2,8 @@ import { reverse } from "./challenges/reverse-string";
 import { palindrome } from "./challenges/palindrome";
 import { reverseInt } from "./challenges/reverse-int";
 import { maxChar } from "./challenges/maxchar";
+import { fizzBuzz } from "./challenges/fizzbuzz";
+
+fizzBuzz(15);
 
 console.log("Successfully!");
